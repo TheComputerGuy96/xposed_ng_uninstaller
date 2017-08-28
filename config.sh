4d057c517edc91a43029c08ae5ae6e0301e70523
DodoGTA GT <vwestlife20@gmail.com>
2017-08-28 19:19:45 +0300

    Adjust stuff for my Magisk module

--- config.sh
+++ config.sh
@@ -18,32 +18,15 @@
 # NOTE: This part has to be adjusted to fit your own needs
 # This will be the folder name under "/magisk"
 # This should also be the same as the ID in your "config.sh" & "module.prop" of your Magisk Module
-MODID=template
+MODID=xposed_ng
   
 ##########################################################################################
 # UnInstallation Message
 ##########################################################################################
   
 # Set what you want to show when Uninstalling your MOD
-print_modname()
-{
-  ui_print "************************************"
-  ui_print " Magisk Module Uninstaller Template "
-  ui_print "************************************"
-}
-  
-##########################################################################################
-# UnInstallation Extra Function , to be Used Only IF Needed
-##########################################################################################
-  
-# Will be Executed Before UnInstallation
-script_before_uninstall()
-{
-  # Add script Here
-}
-  
-# Will be Executed After UnInstallation
-script_after_uninstall()
-{
-  # Add script Here
+print_modname() {
+  ui_print "*********************************"
+  ui_print "Xposed framework uninstaller ZIP "
+  ui_print "*********************************"
 }
